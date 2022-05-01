e00e01db6f9fe36940c352d40c142e2c3e5aab6b
iTNTPiston <zhaoyizhu2000@gmail.com>
2022-04-30 18:11:06 -0700

    Hotfix replace forward slash with back slash in ps1

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,4 +1,3 @@
-
 Write-Output "=== Celer Devtool Setup Script ==="
 $ARCH = "x86_64"
 $OS = "windows"
@@ -24,7 +23,7 @@ if ($CelerDNEError) {
 
 if ($OPTION_NUM -eq "1"){
   if ($BIN_DIR -eq $null) {
-    $DEFAULT_INSTALL_PATH="~/.celer"
+    $DEFAULT_INSTALL_PATH="$HOME\.celer"
   }else{
     $DEFAULT_INSTALL_PATH=$BIN_DIR
   }
@@ -68,20 +67,3 @@ if ($OPTION_NUM -eq "1"){
   Set-Content -Path $PSHOME\Profile.ps1 -Value (Get-Content -Path $PSHOME\Profile.ps1 | Select-String -Pattern '# Add Celer to Path$' -NotMatch)
   Write-Output "Done"
 }
-
-
-
-
-
-# else
-
-
-# # uninstall
-# # which celer (sh) or Get-Command celer to find out install path
-# # (windows) read powershell $PSHOME
-# # (windows) Get-Content/Set-Content for ps to remove path
-# # delete CELER_PATH/celer 
-#   echo "Deleting $BIN"
-#   rm $BIN
-#   echo "Done"
-# fi
