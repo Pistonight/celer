024b049fadedb433c7a274089af360c5e5893faf
iTNTPiston <44533763+iTNTPiston@users.noreply.github.com>
2022-08-25 22:42:32 +0000

    Deploying to prod from @ iTNTPiston/celer@8523c75bee22ab18def427e86c2635ba30d92062 🚀

--- setup.sh
+++ setup.sh
@@ -1,33 +1,44 @@
 #!usr/bin/bash
 
+# This is the script for both linux and mac
 echo "=== Celer Devtool Setup Script ==="
+
+# Get OS info
 ARCH=$(uname -m)
-OS=$(uname -s | tr '[:upper:]' '[:lower:]') # Darwin for mac, Linux for linux
+OS=$(uname -s | tr '[:upper:]' '[:lower:]') # darwin for mac, linux for linux
 if [[ ! $ARCH == x86_64 ]]
 then
   echo "Your architecture is currently not supported. Only x86_64 is supported for now."
   exit 1
 fi
-echo
 
-PS3="Choose an option (1 or 2): "
-OPTION_INSTALL="Install/Update"
-OPTION_UNINSTALL="Uninstall"
-
-select OPTION in $OPTION_INSTALL $OPTION_UNINSTALL
-do
-  if [[ $OPTION == $OPTION_INSTALL ]] || [[ $OPTION == $OPTION_UNINSTALL ]]
-  then
-    break
-  fi
-done
-
+# Get install info
 BIN=$(which celer)
 
 if [[ ! -z $BIN ]]
 then
+  echo
+  # Installation found, prompt update or remove
   BIN_DIR=$(dirname $BIN)
+  PS3="Choose an option (1 or 2): "
+  OPTION_INSTALL="Update"
+  OPTION_UNINSTALL="Uninstall"
+
+  select OPTION in $OPTION_INSTALL $OPTION_UNINSTALL
+  do
+    if [[ $OPTION == $OPTION_INSTALL ]] || [[ $OPTION == $OPTION_UNINSTALL ]]
+    then
+      break
+    fi
+  done
+else
+  OPTION_INSTALL="Install"
+  OPTION=$OPTION_INSTALL
 fi
+
+echo ""
+echo "Confirm: $OPTION celer devtool?"
+read -p "Press Enter to continue or Ctrl + C to abort"
 
 if [[ $OPTION == $OPTION_INSTALL ]]
 then
@@ -35,10 +46,16 @@ then
   then
     DEFAULT_INSTALL_PATH=/usr/local/bin
   else
-    echo "Celer binary is detected at $BIN_DIR"
+    echo "Existing installation is detected at $BIN_DIR"
     DEFAULT_INSTALL_PATH=$BIN_DIR
   fi
-  read -p "Where do you want to install celer? (default: $DEFAULT_INSTALL_PATH) " INSTALL_PATH
+  if [[ $OPTION == "Install" ]]
+  then
+    read -p "Where do you want to install celer? (default: $DEFAULT_INSTALL_PATH) " INSTALL_PATH
+  else
+    read -p "Press Enter to update existing installation or enter a new location: " INSTALL_PATH
+  fi
+
   if [[ -z $INSTALL_PATH ]]
   then
     INSTALL_PATH=$DEFAULT_INSTALL_PATH
@@ -47,18 +64,25 @@ then
     sleep 1
   fi
 
+  # Make sure that target location doesn't contain celer or it's a file (not a directory)
+  if [[ -d "$INSTALL_PATH/celer" ]]
+  then
+    echo "Installation failed: There's a directory in the location named \"celer\", which conflicts with the binary"
+    exit 1
+  fi
+
   URL=$(curl -s https://api.github.com/repos/iTNTPiston/celer/releases/latest \
 | grep "browser_download_url.*-"$ARCH"-"$OS".tar.gz" \
 | cut -d : -f 2,3 \
 | tr -d \")
+  
+  rm -rf $INSTALL_PATH/celer.tar.gz
   echo "Downloading from" $URL
-  wget -q -O temp.tar.gz $URL
+  wget -q -O $INSTALL_PATH/celer.tar.gz $URL
   echo "Extracting"
-  tar -xzf temp.tar.gz
-  rm temp.tar.gz
-  echo "Moving binary"
-  mv celer $INSTALL_PATH
-  echo "Done"
+  tar -xzf $INSTALL_PATH/celer.tar.gz -C $INSTALL_PATH
+  rm $INSTALL_PATH/celer.tar.gz
+  echo "$OPTION complete. run celer --help to verify"
 
 else
   echo "Removing binary at $BIN"
