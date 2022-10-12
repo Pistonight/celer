2df08b0727f75c05e56ea570c503b8db9d86873c
Braden Kattman <kattmabm@rose-hulman.edu>
2022-10-12 13:51:17 -0400

    Boilerplating with old styling method

--- packages/celer-web-app/src/ui/frames/Home.tsx
+++ packages/celer-web-app/src/ui/frames/Home.tsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import { LocalStorageWrapper } from "data/storage";
 import { EmptyObject } from "data/util";
+import { styles } from "./Home.Style";
 
 export const Home: React.FC<EmptyObject> = () => {
 	const [textBundle, setTextBundle] = useState(LocalStorageWrapper.load<string>("TmpBundleString", ""));
-    
+
 	return (
-		<div>
-			<h1>
-                            Home page is working in progress.
-			</h1> 
-			<p>
-                            If you are using the celer dev server, click <a href="#/dev">here</a>
-			</p>
-			<p>
-                            If you are trying to view the route, open the route URL directly. Currently only routes on github are supported. use #/gh/:user/:repo or #/gh/:user/:repo/:branch
-			</p>
-			<hr />
-			<p>
-							Paste the content of bundle.json below or 
-                            upload to view it <input type="file" onChange={(e)=>{
+		<div style={styles.Background}>
+			{/* TODO: arrange title and logo side-by-side */}
+			<div id="div-title">
+				{/* TODO: it may be beneficial to have the link to the image as a variable */}
+				<img src="../celer.png" alt="Celer Logo"/>
+				<h1 style={styles.TitleText}>Celer Route Engine</h1>
+			</div>
+			<div id="recent-routes">
+				<h2 style={styles.SectionTitleText}>My Recent Routes</h2>
+				{/* TODO: apply the RecentRoutesList styles to each list item */}
+				<ul>
+					<li><a href="#/dev">Dev Server</a></li>
+				</ul>
+			</div>
+			<div id="upload-route">
+				<h2 style={styles.SectionTitleText}>Upload Route</h2>
+				<p>To upload a route, please paste the contents of your <span style={styles.CodeBlock}>bundle.json</span> file into the box and press "Submit" or use the "Upload Route" button below.</p>
+				<textarea rows={10} cols={60} value={textBundle} onChange={(e)=>{
+					LocalStorageWrapper.store<string>("TmpBundleString",e.target.value);
+					setTextBundle(e.target.value);
+				}}></textarea>
+				<br/>
+				<input type="file" onChange={(e)=>{
 					const files = e.target.files;
 					if(files?.length && files[0]){
 						const file = files[0];
@@ -29,12 +39,13 @@ export const Home: React.FC<EmptyObject> = () => {
 						});
 					}
 				}}></input>
-			</p>
-			<textarea rows={10} cols={60} value={textBundle} onChange={(e)=>{
-				LocalStorageWrapper.store<string>("TmpBundleString",e.target.value);
-				setTextBundle(e.target.value);
-			}}></textarea>
-			{textBundle && <a href="#/local">View</a>}
+				<br/>
+				<button type="button" id="upload-route-button">Upload Route</button>
+			</div>
+			<div id="gh-route-info">
+				<p>If you have a route on GitHub, you can load it directly through the URL.</p>
+				<p style={styles.CodeBlock}>https://celer.itntpiston.app/#/gh/[user]/[repo]</p>
+			</div>
 		</div>
 	);
 };
