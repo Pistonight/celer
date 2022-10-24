05d8104e91762c9d8d2ed7f59b23eb6b9f43cef4
Braden Kattman <kattmabm@rose-hulman.edu>
2022-10-23 23:48:06 -0400

    Eliminated custom parsing functions

--- packages/celer-web-app/src/data/storage/RecentPages.ts
+++ packages/celer-web-app/src/data/storage/RecentPages.ts
@@ -1,41 +1,35 @@
 import { LocalStorageWrapper } from ".";
 
+// The key in LocalStorage to use
 const KEY = "RecentlyVisitedPages";
-const NUM_PAGES = 5; // <-- Change this to increase number of pages tracked
-const DEFAULT = new Array(NUM_PAGES);
-
-// Input a string, return an array of max length NUM_PAGES with
-//  that string separated by spaces.
-function parseArrayFromString(valueString: string): string[] {
-	const returnArr = valueString.split(" ", NUM_PAGES);
-	return returnArr;
-}
-
-// Input an array, return a string with the elements in the array
-//  joined by spaces.
-function parseStringFromArray(arr: string[]): string {
-	const joinedStr = arr.join(" ");
-	return joinedStr;
-}
+// Maximum number of pages tracked to avoid cluttering home page
+const MAX_PAGES = 5;
 
 // Adds the currently visited page to the recent pages list in localStorage
 export function addPageToRecents(url: string): void {
 	// Load the most recently visited pages
-	let recentlyVisited = LocalStorageWrapper.load(KEY, DEFAULT, parseArrayFromString);
+	let recentlyVisited = loadRecentPages();
 	// Add the current URL to the list of recently visited pages
 	recentlyVisited.splice(0, 0, url);
-	// Remove any duplicate occurrences of the new url from the array
-	for (let i = 1; i < NUM_PAGES; i++) {
+	// Remove any duplicate occurrences of the current from the array
+	let i = 1;
+	for (; i < recentlyVisited.length && i < MAX_PAGES; i++) {
 		if (recentlyVisited[i] === url) {
 			recentlyVisited.splice(i, 1);
 			break;
 		}
 	}
+	// If the array is larger than the maximum size (meaning the list
+	//  is full and no repeats were deleted), delete the last item.
+	if (i == MAX_PAGES) {
+		recentlyVisited.splice(-1, 1);
+	}
 	// Store the updated list back into LocalStorage
-	LocalStorageWrapper.store(KEY, recentlyVisited, parseStringFromArray);
+	LocalStorageWrapper.store(KEY, recentlyVisited);
 }
 
 // Returns the most recently visited pages from localStorage
 export function loadRecentPages(): string[] {
-	return LocalStorageWrapper.load(KEY, DEFAULT, parseArrayFromString);
+	let recentlyVisited = LocalStorageWrapper.load<string[]>(KEY, new Array());
+	return recentlyVisited;
 }
