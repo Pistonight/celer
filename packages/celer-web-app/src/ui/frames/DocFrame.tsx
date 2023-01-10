3ef3e72d4f495c971d35cab69ad7aa88e8dbb3f2
kattmabm <kattmabm@rose-hulman.edu>
2023-01-10 17:15:57 -0500

    Progress checkin

--- packages/celer-web-app/src/ui/frames/DocFrame.tsx
+++ packages/celer-web-app/src/ui/frames/DocFrame.tsx
@@ -5,7 +5,10 @@ import { useStyles } from "ui/StyleContext";
 import { DocLineComponent } from "ui/components";
 import { useAppState } from "core/context";
 import { DocLine, DocLineText, DocLineTextWithIcon } from "core/engine";
-import { useExpScrollProgressTrackerEnabled } from "core/experiments";
+import {
+	useExpScrollProgressTrackerEnabled,
+	useExpCenterToPageEnabled,
+} from "core/experiments";
 import { InGameCoordinates } from "core/map";
 
 export interface DocFrameProps {
@@ -48,6 +51,7 @@ export const DocFrame: React.FC<DocFrameProps> = ({docLines})=>{
 	const [updateHandle, setUpdateHandle] = useState<number|undefined>(undefined);
 	const {setDocCurrentLine, setMapCenter} = useAppState();
 	const ScrollProgressTrackerEnabled = useExpScrollProgressTrackerEnabled();
+	const CenterToPageEnabled = useExpCenterToPageEnabled();
 	const docFrameRef = useRef<HTMLDivElement>(null);
 
 	// Loading styles
@@ -108,6 +112,93 @@ export const DocFrame: React.FC<DocFrameProps> = ({docLines})=>{
 		}
 	}, [docLineRefs]);
 
+	// Center the map such that all steps visible on the document are visible on the map
+	const syncMapToPage = useCallback((scrollPos: number) => {
+		if(!docLineRefs[0].current){
+			return;
+		}
+		// Find the top line that is visible on the document
+		let topLineNum = binarySearchForLine(docLineRefs, scrollPos + docLineRefs[0].current.getBoundingClientRect().top);
+		let lineFound = false;
+		let topLine;
+		// If the top line is an invalid line (header line), check lines below in order
+		for (let lineNumber=topLineNum; lineNumber<docLineRefs.length; lineNumber += 1) {
+			topLine = docLines[lineNumber];
+			if (topLine.lineType === "DocLineText" || topLine.lineType === "DocLineTextWithIcon") {
+				topLineNum = lineNumber;
+				lineFound = true;
+				break;
+			}
+		}
+		// If scanning down does not lead to a valid line, center the map around the first line above
+		if (!lineFound) {
+			for (let lineNumber=topLineNum-1; lineNumber>0; lineNumber -= 1) {
+				topLine = docLines[lineNumber];
+				if (topLine.lineType === "DocLineText" || topLine.lineType === "DocLineTextWithIcon") {
+					setDocCurrentLine(lineNumber);
+					centerMapToLine(topLine, setMapCenter);
+					return;
+				}
+			}
+		}
+		// Find the bottom line that is still visible on the document
+		// TODO: getting bottom line not correct. I suspect something with getBoundingClientRect()
+		let botLineNum = binarySearchForLine(docLineRefs, scrollPos + docLineRefs[0].current.getBoundingClientRect().bottom);
+		let botLine;
+		lineFound = false;
+		for (let lineNumber=botLineNum; lineNumber>topLineNum; lineNumber -= 1) {
+			botLine = docLines[lineNumber];
+			if (botLine.lineType === "DocLineText" || botLine.lineType === "DocLineTextWithIcon") {
+				botLineNum = lineNumber;
+				lineFound = true;
+				break;
+			}
+		}
+		// If the top and bottom lines are the same, center the map around that line
+		if (!lineFound) {
+			topLine = docLines[topLineNum];
+			if (topLine.lineType === "DocLineText" || topLine.lineType === "DocLineTextWithIcon") {
+				setDocCurrentLine(topLineNum);
+				centerMapToLine(topLine, setMapCenter);
+				return;
+			}
+		}
+		console.log("Top Line Number: ", topLineNum);
+		console.log("Bot Line Number: ", botLineNum);
+		// Calculate the minimum and maximum X and Z coordinates
+		topLine = docLines[topLineNum];
+		let lineCoords: InGameCoordinates | undefined;
+		if (topLine.lineType === "DocLineText" || topLine.lineType === "DocLineTextWithIcon") {
+			lineCoords = topLine.centerCoord;
+		}
+		if (!lineCoords) {
+			return;
+		}
+		let minX = lineCoords?.ix;
+		let maxX = lineCoords?.ix;
+		let minZ = lineCoords?.iz;
+		let maxZ = lineCoords?.iz;
+		for (let lineNum = topLineNum; lineNum<=botLineNum; lineNum += 1) {
+			const line = docLines[lineNum];
+			if (line.lineType === "DocLineText" || line.lineType === "DocLineTextWithIcon") {
+				lineCoords = line.centerCoord;
+				if (!lineCoords) {
+					continue;
+				}
+				const x = lineCoords.ix;
+				const z = lineCoords.iz;
+				minX = Math.min(x, minX);
+				maxX = Math.max(x, maxX);
+				minZ = Math.min(z, minZ);
+				maxZ = Math.max(z, maxZ);
+			}
+		}
+		console.log("Min X: ", minX);
+		console.log("Max X: ", maxX);
+		console.log("Min Z: ", minZ);
+		console.log("Max Z: ", maxZ);
+	}, [docLineRefs]);
+
 	const components:JSX.Element[] = [];
 
 	if (!ScrollProgressTrackerEnabled) {
@@ -143,8 +234,13 @@ export const DocFrame: React.FC<DocFrameProps> = ({docLines})=>{
 						// Calculate the current scroll position
 						// eslint-disable-next-line @typescript-eslint/no-explicit-any
 						const target = e.target as any;
-						// Center the map around the currently selected line
-						syncMapToScrollPos(target.scrollTop || 0);
+						// Center the map around the page if that experiment is enabled
+						if (CenterToPageEnabled) {
+							syncMapToPage(target.scrollTop || 0);
+						} else {
+							// Center the map around the currently selected line
+							syncMapToScrollPos(target.scrollTop || 0);
+						}
 						// Clear the timeout
 						setUpdateHandle(undefined);
 					}, SCROLL_DELAY);
