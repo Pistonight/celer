a5acd3a02e31263effee6b9eba0ca453090b39ff
kattmabm <kattmabm@rose-hulman.edu>
2023-02-09 18:55:20 -0500

    Basic dropdown style

--- packages/celer-web-app/src/ui/components/Settings/SettingsMenuItems/SettingDropdown.Style.ts
+++ packages/celer-web-app/src/ui/components/Settings/SettingsMenuItems/SettingDropdown.Style.ts
@@ -12,26 +12,5 @@ export const DropdownStyle = StyleSheet.create({
         color: "#222222",
     },
 
-    button: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#efefef',
-        height: 50,
-        width: '90%',
-        paddingHorizontal: 10,
-        zIndex: 1,
-    },
-
-    buttonText: {
-        flex: 1,
-        textAlign: 'center',
-    },
-    
-    dropdown: {
-        position: 'absolute',
-        backgroundColor: '#fff',
-        top: 50,
-    },   
-
 
 });
