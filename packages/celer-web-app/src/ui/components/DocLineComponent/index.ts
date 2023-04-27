4959a4c738d91743f1e5d12a87457aa54f775cdf
kattmabm <kattmabm@rose-hulman.edu>
2023-04-27 19:38:20 -0400

    Removed legacy import

--- packages/celer-web-app/src/ui/components/DocLineComponent/index.ts
+++ packages/celer-web-app/src/ui/components/DocLineComponent/index.ts
@@ -2,4 +2,3 @@ export * from "./DocLineComponent";
 export * from "./DocLineSectionComponent.Style";
 export * from "./DocLineBannerComponent.Style";
 export * from "./DocLineTextComponent.Style";
-export * from "./NotesDialog";
